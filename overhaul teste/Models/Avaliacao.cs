2fe6f97a1bd3dafe30fd001c79816219434becd3
giovanavbs <giovana.santos261@etec.sp.gov.br>
2024-11-04 17:53:43 -0300

    att avaliação

---
+++ overhaul teste/Models/Avaliacao.cs
@@ -0,0 +1,21 @@
+﻿namespace overhaul_teste.Models
+{
+    public class Avaliacao
+    {
+        public int IdAvaliacao { get; set; }
+        public int IdPedido { get; set; }
+        public int IdCliente { get; set; }
+        public string AvaliacaoEscrita { get; set; }
+        public decimal AvaliacaoNota { get; set; }
+        public DateTime DataAvaliacao { get; set; }
+
+        public Avaliacao()
+        {
+            IdCliente = Cliente.ClienteLogadoId; 
+           
+        }
+    }
+
+
+
+}
---
+++ overhaul teste/ViewModels/AvaliacaoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using overhaul_teste.Models;
+
+namespace overhaul_teste.ViewModels
+{
+        public class AvaliacaoViewModel
+        {
+            public int IdAvaliacao { get; set; }
+            public string NomeCliente { get; set; }
+            public string SobrenomeCliente { get; set; }
+            public string AvaliacaoEscrita { get; set; }
+            public decimal AvaliacaoNota { get; set; }
+            public DateTime DataAvaliacao { get; set; }
+            public int Quantidade { get; set; }
+            public string MarcaCarro { get; set; }
+            public string ModeloCarro { get; set; }
+            public int AnoCarro { get; set; }
+
+        public List<ItensPedido> ItensPedido { get; set; }
+    }
+    }
+
