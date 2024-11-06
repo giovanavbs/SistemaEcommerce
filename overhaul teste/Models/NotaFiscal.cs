bf960796fc253df868dcabe224724bf60f6ba8de
giovanavbs <giovana.santos261@etec.sp.gov.br>
2024-11-06 00:18:29 -0300

    att

---
+++ overhaul teste/Models/NotaFiscal.cs
@@ -0,0 +1,21 @@
+﻿namespace overhaul_teste.Models
+{
+    public class NotaFiscal
+    {
+        public int IdNotaFiscal { get; set; }
+        public int IdPagamento { get; set; }
+        public string NumeroNota { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string FormaPagamento { get; set; }
+
+
+        // armazenar os itens do pedido na nota fiscal
+        public List<ItensPedido> Itens { get; set; } = new List<ItensPedido>();
+
+        public string NomeCliente { get; set; }
+        public string SobrenomeCliente { get; set; }
+        public decimal CpfCnpj { get; set; }
+    }
+
+}
---
+++ overhaul teste/ViewModels/NotaFiscalViewModel.cs
@@ -0,0 +1,17 @@
+﻿using overhaul_teste.Models;
+
+namespace overhaul_teste.ViewModels
+{
+    public class NotaFiscalViewModel
+    {
+        public int IdNotaFiscal { get; set; }
+        public string NumeroNota { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public decimal ValorTotal { get; set; }
+        public string FormaPagamento { get; set; }
+        public List<ItensPedido> Itens { get; set; } = new List<ItensPedido>();
+        public string NomeCliente { get; set; }
+        public string SobrenomeCliente { get; set; }
+        public decimal CpfCnpj { get; set; }
+    }
+}
